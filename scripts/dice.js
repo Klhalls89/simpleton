5102122eae98ea93b38cb0454e9ca97202d4f820
Kristen Hallstrom <klhalls89@gmail.com>
2019-01-18 12:03:25 -0700

    add dice functionality

---
+++ scripts/dice.js
@@ -0,0 +1,6 @@
+function diceRoll() {
+  let genNum = Math.ceil(Math.random() * (6 - 0) + 0);
+  changeDisplayNum(genNum)
+  // moveDisplayedBoarder(1)
+
+}
---
+++ scripts/dom.js
@@ -0,0 +1,10 @@
+
+document.querySelector('.rollBtnWord').addEventListener('click', diceRoll);
+
+function changeDisplayNum(num) {
+  document.querySelector('.diceDisplayNum').innerText = num
+}
+
+// function moveDisplayedBoarder(num) {
+
+// }
